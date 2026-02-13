1dc3f32d97d32755063e56e2a75bd3ccc9c13016
Flazened <ytmichaelyusliardy88@gmail.com>
2026-02-13 11:38:15 +0700

    update:"change homepage code"

--- public/index.php
+++ public/index.php
@@ -8,7 +8,13 @@ $title = "ArtVault - Home";
 <head>
     <meta charset="UTF-8">
     <title><?= $title; ?></title>
-    <link rel="stylesheet" href="assets/css/homepage.css">
+    <link rel="stylesheet" href="assets/css/s.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@400..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
 
@@ -20,10 +26,9 @@ $title = "ArtVault - Home";
 
     <ul class="nav-menu">
         <li><a href="index.php">Home</a></li>
-        <li><a href="about.php">About</a></li>
-        <li><a href="features.php">Features</a></li>
-        <li><a href="gallery.php">Gallery</a></li>
-        <li><a href="contact.php">Contact</a></li>
+        <li><a href="about.php">Gallery</a></li>
+        <li><a href="features.php">About</a></li>
+        <li><a href="gallery.php">Contact</a></li>
     </ul>
 
     <div class="nav-right">
@@ -33,14 +38,10 @@ $title = "ArtVault - Home";
 </nav>
 
 <!-- HERO / HOME SECTION -->
+ <div class="B1"><h1>.</h1></div>
 <section class="hero">
-    <img src="assets/image/banner.png" class="hero-img" alt="Banner">
+    <img src="assets/image/Banner-Top.png" class="hero-img" alt="Banner">
 
-    <div class="hero-overlay">
-        <h1>ARTVAULT</h1>
-        <p>Website that holds art made by our students.</p>
-    </div>
-</section>
 
 <script src="assets/js/home.js"></script>
 </body>
