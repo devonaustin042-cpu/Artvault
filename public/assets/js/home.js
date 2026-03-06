f77fda998eba54ce8de8e421423a8c9349b227de
Flazened <ytmichaelyusliardy88@gmail.com>
2026-03-06 10:41:13 +0700

    delete

--- public/assets/js/home.js
+++
@@ -1,8 +0,0 @@
-// contoh interaksi sederhana
-document.querySelector(".login").addEventListener("click", function () {
-    alert("Login clicked!");
-});
-
-document.querySelector(".signup").addEventListener("click", function () {
-    alert("Sign Up clicked!");
-});
