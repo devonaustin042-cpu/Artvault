8c363f2c6b048bda0d04c2272b50ad7bd4fb69fc
Flazened <ytmichaelyusliardy88@gmail.com>
2026-05-01 22:48:06 +0700

    fix: dude chill, do pull first plsss, my code now is crying on the side of hell..... fixing all code, landing, css, and entire system that has been destroyed

--- app/views/landing/gallery.php
+++ app/views/landing/gallery.php
@@ -78,23 +78,87 @@
         </div>
     </div>
 
-    <!-- FOOTER -->
-    <footer class="footer">
-        <div class="footer-main">
-            <div class="footer-col">
-                <h4 class="footer-heading">Navigation</h4>
-                <ul class="footer-links">
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/gallery">Gallery</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
+         <!-- FOOTER --> 
+
+          <!-- Brand -->
+            <footer class="footer">
+                <div class="footer-brand">
+                        <div class="footer-brand-top">
+                            <img src="/img/logo/Artvault-white.png" alt="Artvault Logo" class="footer-logo">
+                            <span class="footer-brand-name">Artvault</span>
+                        </div>
+                    </div>
+            <div class="footer-main">
+                
+
+                <!-- Navigation -->
+                <div class="footer-col">
+                    <h4 class="footer-heading">Navigation</h4>
+                    <ul class="footer-links">
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/gallery">Gallery</a></li>
+                        <li><a href="/about">About</a></li>
+                        <li><a href="/contact">Contact</a></li>
+                    </ul>
+                </div>
+
+                <!-- Contact -->
+                <div class="footer-col">
+                    <h4 class="footer-heading">Contact</h4>
+                    <ul class="footer-contact">
+                        <li>
+                            <span class="contact-icon">
+                                <img src="/img/icon/mail.png" alt="Email Icon" class="contact-icon-img">
+                            </span>
+                            <span>Artvault@gmail.com</span>
+                        </li>
+                        <li>
+                            <span class="contact-icon">
+                                <img src="/img/icon/location.png" alt="Location Icon" class="contact-icon-img">
+                            </span>
+                            <span>Pontianak, Kalimantan Barat, Indonesia</span>
+                        </li>
+                        <li>
+                            <span class="contact-icon">
+                                <img src="/img/icon/telephone.png" alt="Phone Icon" class="contact-icon-img">
+                            </span>
+                            <span>+62 897 3871 170</span>
+                        </li>
+                    </ul>
+                </div>
+
+                <!-- Help & Center -->
+                <div class="footer-col">
+                    <h4 class="footer-heading">Help & Center</h4>
+                    <ul class="footer-links">
+                        <li><a href="#">Customer Support</a></li>
+                        <li><a href="#">Terms and Services</a></li>
+                        <li><a href="#">Privacy Policy</a></li>
+                        <li><a href="#">Cancellation and Refund Policy</a></li>
+                    </ul>
+                </div>
+
+                <!-- Logo + Sosmed -->
+                <div class="footer-col footer-social-col">
+                    <img src="/img/logo/Artvault-white.png" alt="Artvault" class="footer-logo-big">
+                    <div class="footer-social">
+                        <p class="footer-follow">Follow Us</p>
+                        <div class="social-icons">
+                            <a href="#"><img src="/img/icon/instagram.png" alt="Instagram"></a>
+                            <a href="#"><img src="/img/icon/facebook.png" alt="Facebook"></a>
+                            <a href="#"><img src="/img/icon/tiktok.png" alt="TikTok"></a>
+                            <a href="#"><img src="/img/icon/youtube.png" alt="YouTube"></a>
+                        </div>
+                    </div>
+                </div>
+
             </div>
-        </div>
-        <div class="footer-bottom">
-            <p>Copyright &copy; 2025 Artvault All Rights Reserved</p>
-        </div>
-    </footer>
+
+            <!-- Bottom Bar -->
+            <div class="footer-bottom">
+                <p>Copyright &copy; 2025 Artvault All Rights Reserved</p>
+            </div>
+        </footer>
     <script src="/js/script.js"></script>
 </body>
 </html>
